08514101c3a2d71fc0c26e21e1782c0291058670
pettyferlove@live.cn <pettyferlove@live.cn>
2018-08-14 07:31:57 +0800

    init

---
+++ answer/api/OcrApi.py
@@ -0,0 +1,47 @@
+import urllib.request
+import urllib.parse
+import base64
+import time
+import uuid
+import hashlib
+import json
+import logging
+import tkinter.messagebox
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s Process%(process)d:%(thread)d %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='../logs/OcrApi.log',
+                    filemode='a')
+
+
+def query_api(data):
+    global text
+    nonce_str = str(uuid.uuid4()).replace('-', '')
+    data = base64.b64encode(data)
+    app_id = 2107761575
+    app_key = '2qmvi0sY5vRb3PAO'
+    temp = [('app_id', app_id), ('time_stamp', int(time.time())), ('nonce_str', nonce_str),
+            ('image', data)]
+    temp.sort()
+    temp.append(('app_key', app_key))
+    temp = dict(temp)
+    temp_str = urllib.parse.urlencode(temp)
+    md5_str = hashlib.md5(temp_str.encode('UTF-8')).hexdigest().upper()
+    del temp['app_key']
+    temp['sign'] = md5_str
+    data = urllib.parse.urlencode(temp).encode('UTF-8')
+    logging.debug('Request----->' + str(temp))
+    request = urllib.request.Request(r'https://api.ai.qq.com/fcgi-bin/ocr/ocr_generalocr', data)
+    response = urllib.request.urlopen(request)
+    result = response.read().decode('utf-8')
+    temp = json.loads(result)
+    logging.debug('Response----->' + str(temp))
+    if temp['ret'] != 0:
+        print('错误！可能是截图出错或者响应时间超过五秒。')
+        tkinter.messagebox.showerror('错误', '可能是截图出错或者响应时间超过五秒')
+    else:
+        text = ''
+        for item in temp['data']['item_list']:
+            text += item['itemstring']
+    return text
--- answer/view/MainWin.py
+++ answer/view/MainWin.py
@@ -1,21 +1,32 @@
 import tkinter
 from PIL import ImageGrab
-import win32api
-
-def hello_call_back():
-    print("Hello Python", "Hello Runoob")
+from PIL import Image
+from win32api import GetSystemMetrics
+from answer.api.OcrApi import query_api
+from io import BytesIO
 
 
 def init_view():
     window = tkinter.Tk()
     window.title("剑网三自动答题器")
-    window.geometry("700x400")
-    btn_one = tkinter.Button(text="sss", command=hello_call_back)
+    window.geometry("400x200")
     btn_print_screen = tkinter.Button(text="截取屏幕", command=print_screen)
-    btn_one.pack()
-    btn_print_screen.pack()
+    btn_print_screen.grid(row=4, column=2, columnspan=3)
     window.mainloop()
 
 
 def print_screen():
-    ImageGrab.grab((300, 100, 1400, 600)).show()
+    img = ImageGrab.grab((posx1, posy1, posx2, posy2))
+    byte_data = BytesIO()
+    img.save(byte_data, format="JPEG")
+    byte_data = byte_data.getvalue()
+    text = query_api(byte_data)
+    print(text)
+
+
+posx1 = int(GetSystemMetrics(0) * 0.040625)
+posy1 = int(GetSystemMetrics(1) * 0.25)
+posx2 = int(GetSystemMetrics(0) * 0.410625)
+posy2 = int(GetSystemMetrics(1) * 0.32)
+showx = int(GetSystemMetrics(0) * 0.05625)
+showy = int(GetSystemMetrics(1) * 0.5)
