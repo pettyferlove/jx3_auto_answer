4d5cd257e1dc573be109aa10f47f58674a5f9326
Petty <liuyang@minstone.com.cn>
2018-08-15 10:08:38 +0800

    init

--- answer/view/MainWin.py
+++ answer/view/MainWin.py
@@ -1,34 +1,23 @@
 import tkinter
-from PIL import ImageGrab
-from answer.common.data import qst
-import win32api
-from win32api import GetSystemMetrics
-from answer.api.OcrApi import query_api
-from io import BytesIO
+from answer.common.Data import qst
+from answer.common.BindKey import BindKey, register_lab
+
+lab_message = None
 
 
 def init_view():
+    global lab_message
     print(qst[0])
     window = tkinter.Tk()
     window.title("剑网三自动答题器")
     window.geometry("400x200")
-    btn_print_screen = tkinter.Button(text="截取屏幕", command=print_screen)
-    btn_print_screen.grid(row=4, column=2, columnspan=3)
+    lab_message = tkinter.Label(window, text='\n\n欢迎使用剑网三智能答题器')
+    btn_print_screen = tkinter.Button(window, text="截取屏幕", command=print_screen, width=20, height=2)
+    lab_message.pack()
+    btn_print_screen.pack()
     window.mainloop()
 
 
 def print_screen():
-    img = ImageGrab.grab((posx1, posy1, posx2, posy2))
-    byte_data = BytesIO()
-    img.save(byte_data, format="JPEG")
-    byte_data = byte_data.getvalue()
-    text = query_api(byte_data)
-    print(text)
-
-
-posx1 = int(GetSystemMetrics(0) * 0.040625)
-posy1 = int(GetSystemMetrics(1) * 0.25)
-posx2 = int(GetSystemMetrics(0) * 0.410625)
-posy2 = int(GetSystemMetrics(1) * 0.32)
-showx = int(GetSystemMetrics(0) * 0.05625)
-showy = int(GetSystemMetrics(1) * 0.5)
+    register_lab(lab_message)
+    BindKey().start()
